4db23fd95adb2ee25598882b7429c4ac20f6204c
Yamil <issayamilrt@gmail.com>
2025-03-03 14:37:31 +0100

    fix: accuracy

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-
+from sklearn.linear_model import LogisticRegression
 data = pd.read_csv('tested.csv')
 
 print(data.head())
@@ -30,15 +30,20 @@ data.drop(['PassengerId', 'Name', 'Ticket'], axis=1, inplace=True)
 X = data.drop('Survived', axis=1)
 y = data['Survived']
 
+X = X.drop('Sex', axis=1)
+
+
 X.fillna(X.median(), inplace=True)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-
+model = LogisticRegression(max_iter=200)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy:.2f}')
 
 conf_matrix = confusion_matrix(y_test, y_pred)
 sns.heatmap(conf_matrix, annot=True, fmt='d')
