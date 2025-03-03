3439bdcbdde89944fe6b2c0d0b0874f97b98508d
Yamil <issayamilrt@gmail.com>
2025-03-03 13:52:05 +0100

    initialize model

---
+++ model.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+data = pd.read_csv('tested.csv')
+
+print(data.head())
+print(data.isnull().sum())
+
+sns.histplot(data['Age'].dropna(), kde=True)
+plt.show()
+
+sns.countplot(x='Pclass', hue='Survived', data=data)
+plt.show()
+
+data['Age'].fillna(data['Age'].median(), inplace=True)
+data['Embarked'].fillna(data['Embarked'].mode()[0], inplace=True)
+data.drop('Cabin', axis=1, inplace=True)
+
+label_encoder = LabelEncoder()
+data['Sex'] = label_encoder.fit_transform(data['Sex'])
+data['Embarked'] = label_encoder.fit_transform(data['Embarked'])
+
+data.drop(['PassengerId', 'Name', 'Ticket'], axis=1, inplace=True)
+X = data.drop('Survived', axis=1)
+y = data['Survived']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy:.2f}')
+
+conf_matrix = confusion_matrix(y_test, y_pred)
+sns.heatmap(conf_matrix, annot=True, fmt='d')
+plt.show()
+
+print(classification_report(y_test, y_pred))
