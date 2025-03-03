1dc6639e05b6f8e62887015243d6690c9a6f0819
Yamil <issayamilrt@gmail.com>
2025-03-03 14:15:27 +0100

    fix: remove Nan values

--- model.py
+++ model.py
@@ -30,6 +30,8 @@ data.drop(['PassengerId', 'Name', 'Ticket'], axis=1, inplace=True)
 X = data.drop('Survived', axis=1)
 y = data['Survived']
 
+X.fillna(X.median(), inplace=True)
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 model = RandomForestClassifier(n_estimators=100, random_state=42)
@@ -37,9 +39,6 @@ model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
-
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Accuracy: {accuracy:.2f}')
 
 conf_matrix = confusion_matrix(y_test, y_pred)
 sns.heatmap(conf_matrix, annot=True, fmt='d')
