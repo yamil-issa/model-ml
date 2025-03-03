bfbcf98c8727600c66093038ca80f200ea7eeb01
Yamil <issayamilrt@gmail.com>
2025-03-03 14:59:33 +0100

    remove unused import

--- model.py
+++ model.py
@@ -1,10 +1,8 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.linear_model import LogisticRegression
 data = pd.read_csv('tested.csv')
